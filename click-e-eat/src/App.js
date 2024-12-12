f00189c102a925ce3f9379cb996407ee7a5c955b
guustavuo <gustavo.nardi@aluno.ifsp.edu.br>
2024-12-12 09:27:45 -0300

    alterações páginas

--- click-e-eat/src/App.js
+++ click-e-eat/src/App.js
@@ -2,16 +2,25 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Restaurants from "./components/Restaurants";
+import Cadastro from "./components/Cadastro";
+import Cardapio from "./components/Cardapio";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Página de Login */}
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Página de Cadastro */}
+        <Route path="/cadastro" element={<Cadastro />} />
+
+        {/* Página de Cardapio */}
+        <Route path="/cardapio" element={<Cardapio />} />
+
 
         {/* Página de Restaurantes */}
-        <Route path="/Restaurants" element={<Restaurants />} />
+        <Route path="/" element={<Restaurants />} />
       </Routes>
     </Router>
   );
